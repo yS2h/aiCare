4cee774054ad3150747fc1a064281a61926477c5
sue <ysh@knu.ac.kr>
2025-08-04 16:47:56 +0900

    feat: 프론트엔드 초기 설정

---
+++ fe/src/pages/About/index.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <div className="text-xl font-bold p-4">소개 페이지</div>;
+}
---
+++ fe/src/pages/Home/index.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div className="text-xl font-bold p-4">홈 페이지</div>;
+}
