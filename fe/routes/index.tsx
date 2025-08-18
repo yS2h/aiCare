42fa783407e357b378b086e0ba423f01c014bc24
go-minjung <a01086712346@gmail.com>
2025-08-18 11:24:14 +0900

    feat: route 설정 정리

---
+++ fe/routes/index.tsx
@@ -0,0 +1,37 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from '@/pages/Home'
+import MyPage from '@/pages/MyPage'
+import Login from '@/pages/Login'
+import GrowthHistory from '@/pages/GrowthHistory'
+import BoneAge from '@/pages/BoneAge'
+import Spine from '@/pages/Spine'
+import Guide from '@/pages/Guide'
+import Consulting from '@/pages/Consulting'
+import Information from '@/pages/Information'
+import ProtectedRoute from './ProtectedRoute'
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      {/* 게스트 전용 */}
+      <Route element={<ProtectedRoute mode="guest" />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
+
+      {/* 로그인 필요 */}
+      <Route element={<ProtectedRoute mode="auth" />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/growth-history" element={<GrowthHistory />} />
+        <Route path="/information" element={<Information />} />
+        <Route path="/bone-age" element={<BoneAge />} />
+        <Route path="/spine" element={<Spine />} />
+        <Route path="/guide" element={<Guide />} />
+        <Route path="/consulting" element={<Consulting />} />
+      </Route>
+
+      {/* 알 수 없는 경로는 홈으로 */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
+}
--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,28 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import MyPage from './pages/MyPage'
-import Login from './pages/Login'
-import GrowthHistory from './pages/GrowthHistory'
-import BoneAge from './pages/BoneAge'
-import Spine from './pages/Spine'
-import Guide from './pages/Guide'
-import Consulting from './pages/Consulting'
-import Information from './pages/Information'
+import AppRoutes from '../routes'
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/mypage" element={<MyPage />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/growth-history" element={<GrowthHistory />} />
-      <Route path="/information" element={<Information />} />
-      <Route path="/bone-age" element={<BoneAge />} />
-      <Route path="/spine" element={<Spine />} />
-      <Route path="/guide" element={<Guide />} />
-      <Route path="/consulting" element={<Consulting />} />
-    </Routes>
-  )
+  return <AppRoutes />
 }
 
 export default App
