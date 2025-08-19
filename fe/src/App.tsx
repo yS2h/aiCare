1f99c3937516b7eeb78965e17ca10620e7f9743d
go-minjung <a01086712346@gmail.com>
2025-08-19 15:40:05 +0900

    feat: QueryClientProvider 수정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,21 @@
 import AppRoutes from '../routes'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30_000,
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function App() {
-  return <AppRoutes />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />
+    </QueryClientProvider>
+  )
 }
 
 export default App
