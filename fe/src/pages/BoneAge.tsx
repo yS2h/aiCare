fc374d6fa1a42120a6b44586cd503587e1950e45
go-minjung <a01086712346@gmail.com>
2025-08-22 09:25:23 +0900

    chore: Button 컴포넌트 삭제

--- fe/src/pages/BoneAge.tsx
+++ fe/src/pages/BoneAge.tsx
@@ -1,6 +1,5 @@
 import TopBar from '../components/Topbar'
 import BottomNav from '../components/BottomNav'
-import Button from '../components/Button'
 
 export default function BoneAge() {
   return (
@@ -8,8 +7,6 @@ export default function BoneAge() {
       <TopBar title="골연령 및 골격성숙도" variant="light" />
 
       <div className="flex-1 px-4 pt-4 pb-24">{/* TODO: 필요한 콘텐츠 여기에 추가 */}</div>
-
-      <Button label="새로 분석하기" />
 
       <BottomNav activePage="/bone-age" />
     </div>
